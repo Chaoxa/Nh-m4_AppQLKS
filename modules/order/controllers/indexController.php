7abe0941c409da2da0c6f6e956e018d10d0583b3
Chaoxa <quytran52003@gmail.com>
2023-03-04 11:41:09 +0700

    04/03

--- modules/order/controllers/indexController.php
+++ modules/order/controllers/indexController.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Arr;
+
 function construct()
 {
     //    echo "DÙng chung, load đầu tiên";
@@ -8,5 +10,37 @@ function construct()
 
 function checkoutAction()
 {
-    load_view('checkout');
+    $id = $_GET['id'];
+    $room = db_fetch_array("SELECT * FROM `tbl_room` WHERE `room_id` = $id");
+    // show_array($_POST);
+    if (isset($_POST['btn-order'])) {
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $time = date("d/m/Y") . ' | ' . date('H:i');
+        $order_code = 'gr4#' . date("His");
+        if (!empty($_POST['fullname'])) {
+            $fullname = $_POST['fullname'];
+        }
+        if (!empty($_POST['tel'])) {
+            $tel = $_POST['tel'];
+        }
+        if (!empty($_POST['email'])) {
+            $email = $_POST['email'];
+        }
+        $id_guest = info_user('users_id');
+        $data = array(
+            'fullname' => $fullname,
+            'tel' => $tel,
+            'email' => $email
+        );
+        db_update('guest', $data, "`users_id`= $id_guest");
+        $data_room_order = array(
+            'guest_id' => $id_guest,
+            'room_id' => $id,
+            'time' => $time
+        );
+        $_SESSION['order_info'] = $data_room_order;
+        db_insert('tbl_order_room', $data_room_order);
+    }
+    $data['room'] = $room;
+    load_view('checkout', $data);
 }
--- modules/order/views/checkoutView.php
+++ modules/order/views/checkoutView.php
@@ -17,7 +17,7 @@
                         </div>
                         <div class="form-group">
                             <label for="email">Email:</label>
-                            <input type="email" name="fullname" placeholder="nhom4abc@gmail.com" class="form-control" id="email">
+                            <input type="email" name="email" placeholder="nhom4abc@gmail.com" class="form-control" id="email">
                             <span class="form-message"></span>
                         </div>
                     </div>
@@ -25,27 +25,29 @@
                 <div class="col-md-6">
                     <div class="room-hotel">
                         <div class="row">
-                            <div class="thumb-main">
-                                <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/415261418.jpg?k=466cdf1cc8fda37f74fc5b33270f05491a9be5b39261b2d55847fb6ac076a3ef&o=&hp=1" alt="">
-                            </div>
-                            <div class="more-info">
-                                <h2 class="text-white">Khách sạn Đà Nẵng</h2>
-                                <div class="vote"><i class="bi bi-star-fill text-warning"></i><i class="bi bi-star-fill text-warning"></i><i class="bi bi-star-fill text-warning"></i><i class="bi bi-star-fill text-warning"></i><i class="bi bi-star-fill text-warning"></i></div>
-                                <div class="freeship my-1 text-white"><i class="bi bi-truck-front-fill text-dark"></i>Miễn
-                                    phí
-                                    bãi đậu
-                                    xe</div>
-                                <div class="price">
-                                    <div class="row">
-                                        <div class="money text-success">
-                                            <b> 560.000VNĐ</b>
-                                        </div>
-                                        <div class="coin px-2">
-                                            <b>560 coin</b>
+                            <?php foreach ($room as $value) { ?>
+                                <div class="thumb-main">
+                                    <img src="<?php echo $value['thumb_main_room'] ?>" alt="">
+                                </div>
+                                <div class="more-info">
+                                    <h2 class="text-white"><?php echo $value['room_name'] ?></h2>
+                                    <div class="vote"><i class="bi bi-star-fill text-warning"></i><i class="bi bi-star-fill text-warning"></i><i class="bi bi-star-fill text-warning"></i><i class="bi bi-star-fill text-warning"></i><i class="bi bi-star-fill text-warning"></i></div>
+                                    <div class="freeship my-1 text-white"><i class="bi bi-truck-front-fill text-dark"></i>Miễn
+                                        phí
+                                        bãi đậu
+                                        xe</div>
+                                    <div class="price">
+                                        <div class="row">
+                                            <div class="money text-success">
+                                                <b><?php echo price($value['new_price'], 'VNĐ') ?></b>
+                                            </div>
+                                            <div class="coin px-2">
+                                                <b><?php echo price($value['new_price'] / 1000, 'Coin') ?></b>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            <?php } ?>
                         </div>
                     </div>
                     <div class="payments">
@@ -61,8 +63,8 @@
                     </div>
                 </div>
             </div>
-            <input type="submit" name="btn-order" hidden>
-            <input type="submit" class="btn btn-success btn-order my-5" value="Đặt phòng">
+            <input type="text" name="btn-order" hidden>
+            <input type="submit" class="btn btn-success btn-order my-5" value="Đặt phòng" name="btn-order">
         </form>
         <script src="public/js/validation.js"></script>
         <script>
